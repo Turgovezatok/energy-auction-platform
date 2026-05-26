2490a5b6249f233b28f335c741db747f5b307fe9
Turgovezatok <info@thermoodit.com>
2026-05-26 11:59:05 +0300

    Update page.tsx

--- app/market/buy/page.tsx
+++ app/market/buy/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { createClient } from "@/lib/supabase/client";
+import { supabase } from "../../../lib/supabase";
 
 const supabase = createClient();
 
