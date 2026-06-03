db57a092c4d40e096f722a4659ce4b343e546d09
Turgovezatok <info@thermoodit.com>
2026-06-03 12:23:19 +0300

    Update import-auer-register.ts

--- import-auer-register.ts
+++ import-auer-register.ts
@@ -2,7 +2,7 @@ import * as XLSX from "xlsx";
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: ".env.local" });
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
